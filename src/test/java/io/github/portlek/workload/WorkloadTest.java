4d0234844814805a13f1c34fc104a26cccc5eaa2
Hasan Demirtaş <utsukushihito@outlook.com>
2021-03-19 19:29:25 +0300

    Update WorkloadTest.java

--- src/test/java/io/github/portlek/workload/WorkloadTest.java
+++ src/test/java/io/github/portlek/workload/WorkloadTest.java
@@ -54,6 +54,7 @@ final class WorkloadTest {
   public static void main(final String[] args) throws InterruptedException {
     final WorkloadTest test = new WorkloadTest();
     Executors.newScheduledThreadPool(2).scheduleAtFixedRate(test::exec, 50L, 50L, TimeUnit.MICROSECONDS);
+    Executors.newScheduledThreadPool(2).scheduleAtFixedRate(test.playerTask, 50L, 50L, TimeUnit.MICROSECONDS);
     while (true) {
       Thread.sleep(5L);
     }
